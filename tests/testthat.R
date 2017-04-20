7b0cbc12e354c7119d1230204eeeaa56fa194070
schuemie <schuemie@ohdsi.org>
2017-04-20 07:27:18 -0400

    Trailing deletes...

--- tests/testthat.R
+++
@@ -1,7 +0,0 @@
-# Workaround to fix unit tests in check as described here:
-# https://github.com/hadley/testthat/issues/86
-Sys.setenv(R_TESTS = "")
-
-library(testthat)
-library(CaseCrossover)
-test_check("CaseCrossover")
